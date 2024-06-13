74b207bde62776376a1e96a4945db7defd1c205a
Avais Sethi <avais.sethi@gmail.com>
2024-06-13 12:56:43 -0400

    settings_screen.dart cleanup

--- lib/features/settings/ui/settings_screen.dart
+++ lib/features/settings/ui/settings_screen.dart
@@ -15,11 +15,14 @@ class SettingsScreen extends StatelessWidget {
       switch (state.runtimeType) {
         case SettingsSuccessState:
           state as SettingsSuccessState;
-          return Column(
-            children: [
-              _environmentSelector(state, context),
-              _authToken(state, context),
-            ],
+          return Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: Column(
+              children: [
+                _environmentSelector(state, context),
+                _authToken(state, context),
+              ],
+            ),
           );
         default:
           return const LoadingIndicator();
@@ -28,26 +31,40 @@ class SettingsScreen extends StatelessWidget {
   }
     
   Widget _environmentSelector(SettingsSuccessState state, BuildContext context) {
-    return DropdownButton(
-      value: state.currentEnvironment,
-      items: List.generate(state.environments.length, (index) {
-        var item = state.environments[index];
-        return DropdownMenuItem(value: item, child: Text(item));
-      }),
-      onChanged: (item) {
-        if (item != null) {
-          context.read<SettingsBloc>().selectEnvironment(item);
-        }
-      });
+    return Row(
+      children: [
+        const Text("Current Environment: "),
+        _settingsValue(DropdownButton(
+          value: state.currentEnvironment,
+          items: List.generate(state.environments.length, (index) {
+            var item = state.environments[index];
+            return DropdownMenuItem(value: item, child: Text(item));
+          }),
+          onChanged: (item) {
+            if (item != null) {
+              context.read<SettingsBloc>().selectEnvironment(item);
+            }
+          })),
+      ],
+    );
   }
 
   Widget _authToken(SettingsSuccessState state, BuildContext context) {
-    return TextButton(
-      child: const Text('Tap to copy AuthToken'),
-      onPressed: () async {
-        await Clipboard.setData(ClipboardData(text: state.authToken));
-      }
+    return Row(
+      children: [
+        const Text("Authentication Token: "),
+        _settingsValue(TextButton(
+          child: const Text('Tap to copy'),
+          onPressed: () async {
+            await Clipboard.setData(ClipboardData(text: state.authToken));
+          }
+        )),
+      ],
     );
+  }
+
+  Widget _settingsValue(Widget child) {
+    return SizedBox(width: 100, child: Center(child: child));
   }
     
 }
